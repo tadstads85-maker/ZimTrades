b919458b1718409140dd4a497078718fa25efd8d
tadstads85-maker <tadstads85@gmail.com>
2026-07-16 10:09:53 +0200

    feat: Add UI components index file placeholder

---
+++ packages/ui/index.ts
@@ -0,0 +1,7 @@
+// Re-export UI components
+// This is where shared UI components will be added
+// Examples: Button, Card, Modal, etc.
+
+export const UIComponents = {
+  // Components will be added here
+};
